a492b46c9ef76f99805bf87653441ec3b3286cef
Игорь <igoreshka.first@gmail.com>
2025-07-17 22:41:59 +0200

    Show quote and author separately and modified quotes array of strings to array of objects

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 const quotes = [
-  'The only way to do great work is to love what you do. - Steve Jobs',
-  'Innovation distinguishes between a leader and a follower. - Steve Jobs',
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
-  // Add more quotes as needed
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Strive not to be a success, but rather to be of value',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'The only thing we have to fear is fear itself',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans",
+    author: 'John Lennon',
+  },
+  {
+    quote:
+      'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall',
+    author: 'Nelson Mandela',
+  },
+  {
+    quote:
+      'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    quote: "You miss 100% of the shots you don't take",
+    author: 'Wayne Gretzky',
+  },
 ];
 
 const quoteElement = document.getElementById('quote');
+const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById('generate-btn');
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const { quote, author } = randomQuote;
+  // const quote = randomQuote.quote;
+  // const quoteAuthor = randomQuote.author;
+  // // const quote = `<em>"${randomQuote.quote}"</em><br><span id="author">${randomQuote.author}</span>`;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = author;
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
-
-// Generate a random quote when the page loads
-// generateRandomQuote()

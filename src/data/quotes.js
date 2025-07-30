d67a2daa9b4886ca8e9cb565e74b8cfc78e40578
Игорь <igoreshka.first@gmail.com>
2025-07-30 15:24:03 +0200

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'Strive not to be a success, but rather to be of value',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'The only thing we have to fear is fear itself',
+    author: 'Franklin D. Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: "Life is what happens when you're busy making other plans",
+    author: 'John Lennon',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: 'The greatest glory in living lies not in never falling, but in rising every time we fall',
+    author: 'Nelson Mandela',
+    isFavorite: false,
+  },
+  {
+    text: 'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: "You miss 100% of the shots you don't take",
+    author: 'Wayne Gretzky',
+    isFavorite: false,
+  },
+];
+
+export default quotes;

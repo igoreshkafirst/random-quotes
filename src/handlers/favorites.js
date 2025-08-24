4d928609c45c8f43bfa6e06319a6ca192e5b65ab
Игорь <igoreshka.first@gmail.com>
2025-08-24 19:33:22 +0200

    First steps in synchronizing app state and localStorage (currentQuote and favoriteQuotes)

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,11 +1,12 @@
 import { quoteFavoriteBtn } from '../../index.js';
 
-function toggleFavorite(quote, btn, container) {
-  quote.isFavorite = !quote.isFavorite;
+function toggleFavorite(quote, setCurrentQuote, btn, container) {
+  const shouldToggleIsFavorite = true;
+  setCurrentQuote(quote, shouldToggleIsFavorite);
   toggleFavoriteBtnIcon(quote.isFavorite, btn);
 
   if (quote.isFavorite) {
-    showFavoriteCard(quote, container);
+    showFavoriteCard(quote, setCurrentQuote, container);
   } else {
     removeFavoriteCard(quote.id);
   }
@@ -29,8 +30,9 @@ function hideFavoriteBtn() {
   quoteFavoriteBtn.style.display = 'none';
 }
 
-function removeFavoriteQuote(quote) {
-  quote.isFavorite = false;
+function removeFavoriteQuote(quote, setCurrentQuote) {
+  const shouldToggleIsFavorite = true;
+  setCurrentQuote(quote, shouldToggleIsFavorite);
   removeFavoriteCard(quote.id);
   const currentQuote = document.querySelector('[data-current-quote-id]');
   const currentQuoteId = currentQuote.dataset.currentQuoteId;
@@ -39,7 +41,7 @@ function removeFavoriteQuote(quote) {
   }
 }
 
-function showFavoriteCard(quote, container) {
+function showFavoriteCard(quote, setCurrentQuote, container) {
   const { id, text, author } = quote;
   const favoriteCard = document.createElement('div');
   favoriteCard.classList.add('favorite-card');
@@ -55,12 +57,16 @@ function showFavoriteCard(quote, container) {
   container.appendChild(favoriteCard);
 
   const removeButton = favoriteCard.querySelector('.btn-danger');
-  removeButton.addEventListener('click', () => removeFavoriteQuote(quote));
+  removeButton.addEventListener('click', () =>
+    removeFavoriteQuote(quote, setCurrentQuote)
+  );
 }
 
 function removeFavoriteCard(id) {
   const card = document.querySelector(`[data-favorite-quote-id="${id}"]`);
-  if (card) card.remove();
+  if (card) {
+    card.remove();
+  }
 }
 
-export { handleFavorite, toggleFavorite, hideFavoriteBtn };
+export { handleFavorite, toggleFavorite, hideFavoriteBtn, showFavoriteCard };
--- src/handlers/quote.js
+++ src/handlers/quote.js
@@ -1,8 +1,12 @@
 import { generateRandomInt } from '../utils/math.js';
 import { handleFavorite } from './favorites.js';
 
-function handleQuote(quotes, setCurrentQuote) {
+function handleQuote(quotes, favoriteQuotes, setCurrentQuote) {
   const randomQuote = choseRandomQuote(quotes);
+  // Check if id of randomQuote is among ids of the favorite quotes
+  if (favoriteQuotes.find((quote) => quote.id === randomQuote.id)) {
+    randomQuote.isFavorite = true;
+  }
   setCurrentQuote(randomQuote);
   displayQuote(randomQuote);
 }
@@ -25,4 +29,8 @@ function choseRandomQuote(quotes) {
   return quotes[randomIndex];
 }
 
-export { handleQuote };
+function findQuoteById(quotes, id) {
+  return quotes.find((quote) => quote.id === id);
+}
+
+export { handleQuote, displayQuote, findQuoteById };
